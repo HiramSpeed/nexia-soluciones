23293b7774bccf69394d8e981257bfd6c1eb96b0
Daniel Hiram Navarro <105136354+HiramSpeed@users.noreply.github.com>
2025-12-13 18:19:19 -0600

    Update dashboard, login, and styles for Nexia Solutions

--- condominio-app/proyecto-condominio/config/urls.py
+++ condominio-app/proyecto-condominio/config/urls.py
@@ -1,16 +1,15 @@
 # config/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings # Importa settings
-from django.conf.urls.static import static # Importa static
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Le decimos a Django que para cualquier URL que empiece con 'accounts/',
-    # use las URLs pre-construidas que él ofrece.
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('condominio.urls')), 
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
